047a34f7d6b92fe0dfea0e702062215f141997e0
Onur ALACA <onuralcaa@gmail.com>
2025-03-12 11:17:16 +0300

    App update.

--- SimpleAuth/frontend/src/App.js
+++ SimpleAuth/frontend/src/App.js
@@ -7,7 +7,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Register />} />
+        <Route path="/" element={<Login />} /> {/* Giriş sayfasını varsayılan olarak aç */}
+        <Route path="/register" element={<Register />} /> {/* Kayıt sayfasının yolu değiştirildi */}
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
